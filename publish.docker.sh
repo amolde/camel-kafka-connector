716432a327af6a2ef650af9cd93ae7b3dfae540d
Amol Deshmukh <a.deshmukh@northeastern.edu>
2020-03-11 22:44:45 -0400

    progress

--- publish.docker.sh
+++ publish.docker.sh
@@ -1,3 +1,15 @@
+set -e
+
+# jenv disable-plugin maven
+# jenv enable-plugin maven
+
+jenv global
+# 1.8.0.232
+jenv version
+# 1.8.0.232 (set by /Users/adeshmukh/.jenv/version)
+mvn -version
+mvn --projects custom-package --also-make clean package 
+
 docker build -t amolde/strimzi-kafka-connect .
 docker login
 docker push amolde/strimzi-kafka-connect
