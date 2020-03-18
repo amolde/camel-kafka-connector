af5b4d5b6c17178cf5a63649910e42b1276ceb8f
Amol Deshmukh <a.deshmukh@northeastern.edu>
2020-03-17 21:35:27 -0400

    Salesforce classes generated and compiled

--- publish.docker.sh
+++ publish.docker.sh
@@ -8,7 +8,13 @@ jenv global
 jenv version
 # 1.8.0.232 (set by /Users/adeshmukh/.jenv/version)
 mvn -version
-mvn --projects custom-package --also-make clean package 
+
+. .env
+# cd custom-package
+# mvn camel-salesforce:generate -DcamelSalesforce.clientId=${clientId} -DcamelSalesforce.password=${password} -DcamelSalesforce.userName=${userName} -DcamelSalesforce.clientSecret=${clientSecret}
+
+mvn --projects custom-package --also-make clean package -DcamelSalesforce.clientId=${clientId} -DcamelSalesforce.password=${password} -DcamelSalesforce.userName=${userName} -DcamelSalesforce.clientSecret=${clientSecret}
+# mvn --projects custom-package --also-make package -DcamelSalesforce.clientId=${clientId} -DcamelSalesforce.password=${password} -DcamelSalesforce.userName=${userName} -DcamelSalesforce.clientSecret=${clientSecret}
 
 docker build -t amolde/strimzi-kafka-connect .
 docker login
