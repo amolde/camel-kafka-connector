1d99d259df3b4a47a83483c4bde2d865adeb677c
Amol Deshmukh <a.deshmukh@northeastern.edu>
2020-07-08 20:45:53 -0400

    salesforce jar recreated with new filter

--- publish.docker.sh
+++ publish.docker.sh
@@ -1,5 +1,12 @@
 set -e
 
+docker_tag=$1
+if [[ ${docker_tag} == "" ]]
+then
+    echo "provide docker tag"
+    exit 1
+fi
+
 # jenv disable-plugin maven
 # jenv enable-plugin maven
 
@@ -14,8 +21,14 @@ mvn -version
 # mvn camel-salesforce:generate -DcamelSalesforce.clientId=${clientId} -DcamelSalesforce.password=${password} -DcamelSalesforce.userName=${userName} -DcamelSalesforce.clientSecret=${clientSecret}
 
 # mvn --projects custom-package --also-make clean package -DcamelSalesforce.clientId=${clientId} -DcamelSalesforce.password=${password} -DcamelSalesforce.userName=${userName} -DcamelSalesforce.clientSecret=${clientSecret}
-mvn --projects custom-package --also-make package -DcamelSalesforce.clientId=${clientId} -DcamelSalesforce.password=${password} -DcamelSalesforce.userName=${userName} -DcamelSalesforce.clientSecret=${clientSecret}
+mvn --projects custom-package --also-make clean package \
+    -DcamelSalesforce.clientId=${clientId} \
+    -DcamelSalesforce.version="49.0" \
+    -DcamelSalesforce.loginUrl="${salesforceLoginUrl}" \
+    -DcamelSalesforce.password=${password} \
+    -DcamelSalesforce.userName=${userName} \
+    -DcamelSalesforce.clientSecret=${clientSecret}
 
-docker build -t amolde/strimzi-kafka-connect .
+docker build -t amolde/strimzi-kafka-connect:${docker_tag} -t amolde/strimzi-kafka-connect:latest .
 docker login
 docker push amolde/strimzi-kafka-connect
