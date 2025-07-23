3e59fad82a55d55710adfacf795aa3a8451ec542
Amol Deshmumkh <a.deshmukh@northeastern.edu>
2025-07-23 16:49:37 -0400

    function does not seem to work

--- custom-package/kafka_connect_tls_prepare_certificates.sh
+++ custom-package/kafka_connect_tls_prepare_certificates.sh
@@ -47,11 +47,16 @@ function prepare_truststore {
     done
 }
 
-prepare_truststore "/tmp/cluster.truststore.p12" "$CERTS_STORE_PASSWORD" "/mnt/kafka/external-configuration/kafka-cluster-secrets/ca.crt"
 STORE=/tmp/cluster.keystore.p12
 rm -f "$STORE"
 crtfile=`find /opt/kafka/connect-certs -name 'user.crt' | head -1`
 keyfile=`find /opt/kafka/connect-certs -name 'user.key' | head -1`
+echo "Preparing keystore with ${crtfile} and ${keyfile}..."
 create_keystore_without_ca_file "$STORE" "$CERTS_STORE_PASSWORD" "${crtfile}" "${keyfile}" "user.crt"
+ls -lrt /tmp
+
+echo "Preparing truststore..."
+keytool -keystore "/tmp/cluster.truststore.p12" -storepass "$CERTS_STORE_PASSWORD" -noprompt -alias "cacrt" -import -file "/mnt/kafka/external-configuration/kafka-cluster-secrets/ca.crt" -storetype PKCS12
+ls -lrt /tmp
 
 exec "$@"
